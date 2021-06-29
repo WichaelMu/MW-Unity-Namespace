2f089216503cdd8600675486e395a0897d3ac0bb
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-06-29 20:04:45 +1000

    Generic Heap

--- MW.cs
+++ MW.cs
@@ -2,7 +2,7 @@
 
 namespace MW {
 
-    public enum Direction {
+    public enum EDirection {
         forward,
         right,
         back,
@@ -11,7 +11,7 @@ namespace MW {
         down
     }
 
-    public enum Equation {
+    public enum EEquation {
         Linear,
         EaseInSine,
         EaseOutSine,
@@ -46,7 +46,7 @@ namespace MW {
         EaseInOutElastic,
     };
 
-    public enum Units {
+    public enum EUnits {
         MetresPerSecond,
         KilometresPerSecond,
         MetresPerHour,
@@ -55,6 +55,12 @@ namespace MW {
         MilesPerSecond,
         FeetPerHour,
         MilesPerHour
+    }
+
+    public enum EButton {
+        LeftMouse,
+        MiddleMouse,
+        RightMouse
     }
 
     /// <summary>Generates a new pair of two types of values.</summary>
@@ -85,4 +91,101 @@ namespace MW {
             this.third = third;
         }
     }
+
+    public class THeap<T> where T : IHeapItem<T> {
+        T[] TItems;
+        int nCount;
+
+        public int Count { get {
+                return nCount;
+			} 
+        }
+
+        public THeap(int nMaxSize) {
+            TItems = new T[nMaxSize];
+		}
+
+        public void Add(T TItem) {
+            TItem.nIndex = nCount;
+            TItems[nCount] = TItem;
+
+            SortUp(TItem);
+            nCount++;
+		}
+
+        public T RemoveFirst() {
+            T T_ = TItems[0];
+            nCount--;
+
+            TItems[0] = TItems[nCount];
+            TItems[0].nIndex = 0;
+            SortDown(TItems[0]);
+
+            return T_;
+		}
+
+        public void UpdateItem(T TItem) {
+            SortUp(TItem);
+            SortDown(TItem);
+		}
+
+        public bool Contains(T TItem) {
+            return Equals(TItems[TItem.nIndex], TItem);
+		}
+
+        void SortDown(T TItem) {
+            while (true) {
+                int nLeft = TItem.nIndex * 2 + 1;
+                int nRight = TItem.nIndex * 2 + 2;
+                int nSwap;
+
+                if (nLeft < nCount) {
+                    nSwap = nLeft;
+
+                    if (nRight < nCount) {
+                        if (TItems[nLeft].CompareTo(TItems[nRight]) < 0) {
+                            nSwap = nRight;
+						}
+					}
+
+                    if (TItem.CompareTo(TItems[nSwap]) < 0) {
+                        Swap(TItem, TItems[nSwap]);
+					} else {
+                        return;
+					}
+				} else {
+                    return;
+				}
+            }
+		}
+
+        void SortUp(T TItem) {
+            int nParent = (TItem.nIndex - 1) / 2;
+
+            while (true) {
+                T TParent = TItems[nParent];
+
+                if (TItem.CompareTo(TParent) > 0) {
+                    Swap(TItem, TParent);
+				} else {
+                    break;
+				}
+
+                nParent = (TItem.nIndex - 1) / 2;
+            }
+		}
+
+        void Swap(T l, T r) {
+            TItems[l.nIndex] = r;
+            TItems[r.nIndex] = l;
+
+            int _ = l.nIndex;
+            l.nIndex = r.nIndex;
+            r.nIndex = _;
+		}
+	}
+
+    public interface IHeapItem<T> : System.IComparable<T> {
+        int nIndex { get; set; }
+	}
 }
