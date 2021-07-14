970a1f4d2034c7dc176dde16162f6725a9f2255f
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-07-14 18:44:26 +1000

    Diagnostics
    A stopwatch/timer and a stacktracer.

---
+++ Diagnostics.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace MW.Development {
+
+	public static class Diagnostics {
+
+		public class Stopwatch {
+
+			readonly System.Diagnostics.Stopwatch sw;
+
+			public Stopwatch (bool bStart = false) {
+				sw = new System.Diagnostics.Stopwatch();
+
+				if (bStart) {
+					sw.Start();
+				}
+			}
+
+			public void Start() {
+				sw.Start();
+			}
+
+			public long Stop() {
+				sw.Stop();
+				return sw.ElapsedMilliseconds;
+			}
+
+			public long Restart() {
+				long now = sw.ElapsedMilliseconds;
+				sw.Restart();
+
+				return now;
+			}
+
+			public long Time() {
+				return sw.ElapsedMilliseconds;
+			}
+		}
+
+		public static class Stacktrace {
+
+			public enum EVerbosity { Log, Warning, Error };
+
+			public static void Trace(EVerbosity verbosity = EVerbosity.Log) {
+				switch (verbosity) {
+					case EVerbosity.Log:
+						UnityEngine.Debug.Log(System.Environment.StackTrace);
+						return;
+					case EVerbosity.Error:
+						UnityEngine.Debug.LogError(System.Environment.StackTrace);
+						return;
+					case EVerbosity.Warning:
+						UnityEngine.Debug.LogError(System.Environment.StackTrace);
+						return;
+				}
+			}
+		}
+	}
+
+}
