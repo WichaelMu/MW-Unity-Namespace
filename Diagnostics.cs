a875206ac1010085978276d2dfc9da7353303616
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-07-19 17:05:22 +1000

    More Diagnostics

--- Diagnostics.cs
+++ Diagnostics.cs
@@ -2,46 +2,61 @@
 
 namespace MW.Development {
 
+	/// <summary>Diagnostics for Unity Engine development.</summary>
 	public static class Diagnostics {
 
+		/// <summary>UnityEngine.Debug.[MW.Development.Diagnostics.Stacktrace.EVerbosity]</summary>
+		public enum EVerbosity { Log, Warning, Error };
+
+		/// <summary>Track execution time of code.</summary>
 		public class Stopwatch {
 
 			readonly System.Diagnostics.Stopwatch sw;
 
-			public Stopwatch (bool bStart = false) {
+			/// <summary>Creates a new Stopwatch.</summary>
+			/// <param name="bStartOnCreation">Immediately starting timing upon instantiating?</param>
+			public Stopwatch (bool bStartOnCreation = false) {
 				sw = new System.Diagnostics.Stopwatch();
 
-				if (bStart) {
+				if (bStartOnCreation) {
 					sw.Start();
 				}
 			}
 
-			public void Start() {
-				sw.Start();
-			}
+			/// <summary>Start Stopwatch.</summary>
+			public void Start() => sw.Start();
 
+			/// <summary>Stop Stopwatch and get the elapsed time.</summary>
 			public long Stop() {
 				sw.Stop();
-				return sw.ElapsedMilliseconds;
+				return Time();
 			}
 
+			/// <summary>Restarts Stopwatch and get the time before restarting.</summary>
 			public long Restart() {
-				long now = sw.ElapsedMilliseconds;
+				long now = Time();
 				sw.Restart();
 
 				return now;
 			}
 
-			public long Time() {
-				return sw.ElapsedMilliseconds;
-			}
+			/// <summary>Get the current elapsed time.</summary>
+			public long Time() => sw.ElapsedMilliseconds;
+
+			/// <summary>Get the current elapsed time in seconds.</summary>
+			public long TimeInSeconds() => ToSeconds(Time());
+
+			/// <summary>Converts milliseconds to seconds.</summary>
+			/// <param name="lMilliseconds">Milliseconds to convert.</param>
+			public static long ToSeconds(long lMilliseconds) => lMilliseconds * (long).001;
 		}
 
+		/// <summary>Provides a stacktrace for code.</summary>
 		public static class Stacktrace {
 
-			public enum EVerbosity { Log, Warning, Error };
-
-			public static void Trace(EVerbosity verbosity = EVerbosity.Log) {
+			/// <summary>Stacktrace using UnityEngine.Debug with verbosity.</summary>
+			/// <param name="verbosity">The verbosity of the UnityEngine.Debug.</param>
+			public static void Here(EVerbosity verbosity = EVerbosity.Log) {
 				switch (verbosity) {
 					case EVerbosity.Log:
 						UnityEngine.Debug.Log(System.Environment.StackTrace);
@@ -50,11 +65,53 @@ namespace MW.Development {
 						UnityEngine.Debug.LogError(System.Environment.StackTrace);
 						return;
 					case EVerbosity.Warning:
-						UnityEngine.Debug.LogError(System.Environment.StackTrace);
+						UnityEngine.Debug.LogWarning(System.Environment.StackTrace);
+						return;
+				}
+			}
+
+			/// <summary>Stacktrace using UnityEngine.Debug with verbosity and a message.</summary>
+			/// <param name="sMessage">The message to show.</param>
+			/// <param name="verbosity">The verbosity og the UnityEngine.Debug.</param>
+			public static void Here(string sMessage, EVerbosity verbosity = EVerbosity.Log) {
+				string stacktrace = sMessage + ' ' + System.Environment.StackTrace;
+
+				switch (verbosity) {
+					case EVerbosity.Log:
+						UnityEngine.Debug.Log(stacktrace);
+						return;
+					case EVerbosity.Error:
+						UnityEngine.Debug.LogError(stacktrace);
+						return;
+					case EVerbosity.Warning:
+						UnityEngine.Debug.LogWarning(stacktrace);
 						return;
 				}
 			}
 		}
-	}
 
+		/// <summary>UnityEngine.Logs every object.</summary>
+		/// <param name="debug">The list of objects to log.</param>
+		public static void Log(params object[] debug) {
+			for (int i = 0; i < debug.Length; ++i) {
+				UnityEngine.Debug.Log(debug[i].ToString() + ' ');
+			}
+		}
+
+		/// <summary>UnityEngine.LogErrors every object.</summary>
+		/// <param name="debug">The list of objects to log.</param>
+		public static void LogError(params object[] debug) {
+			for (int i = 0; i < debug.Length; ++i) {
+				UnityEngine.Debug.LogError(debug[i].ToString() + ' ');
+			}
+		}
+
+		/// <summary>UnityEngine.LogWarnings every object.</summary>
+		/// <param name="debug">The list of objects to log.</param>
+		public static void LogWarning(params object[] debug) {
+			for (int i = 0; i < debug.Length; ++i) {
+				UnityEngine.Debug.LogWarning(debug[i].ToString() + ' ');
+			}
+		}
+	}
 }
