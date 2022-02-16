72a30f0d50f4edc61e3a85d849ee3fec84f31b05
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-02-16 12:27:28 +1100

    HasReached and HomeTowards Updates
    HasReached no longer requires bEqualTo to check if two Vector3s.
    
    HomeTowards with a Transform now calls the Vector3 equivalent on it's position, rather than having two almost identical methods.
    
    A 2D version of HomeTowards has been added, using Rigidbody2D.

--- Math/Kinematics.cs
+++ Math/Kinematics.cs
@@ -1,58 +1,66 @@
 ﻿using UnityEngine;
-using MW.Easing;
 
 namespace MW.Kinetic
 {
-	public static class Kinematics {
-        /// <summary>Convert inspector speed to m/s.</summary>
-        public const int kVelocityRatio = 50;
+	public static class Kinematics
+	{
+		/// <summary>Convert inspector speed to m/s.</summary>
+		public const int kVelocityRatio = 50;
 
-        /// <summary>If the distance between from and to is less than or EqualTo detection.</summary>
-        /// <param name="vFrom">The reference coordinate to compare.</param>
-        /// <param name="vTo">The target coordinate to compare.</param>
-        /// <param name="fDistance">The range that is considered if from has 'reached' to.</param>
-        /// <param name="bEqualTo">If this calculation needs to check if the distance between from and to are equal.</param>
-        /// <returns></returns>
-        public static bool HasReached(Vector3 vFrom, Vector3 vTo, float fDistance = .1f, bool bEqualTo = false) {
-            if (bEqualTo)
-                return Vector3.Distance(vFrom, vTo) < fDistance;
-            return Vector3.Distance(vFrom, vTo) <= fDistance;
-        }
+		/// <summary>If the distance between From and To is &lt;= Tolerance.</summary>
+		/// <param name="From">The reference <see cref="Vector3"/> to compare.</param>
+		/// <param name="To">The target <see cref="Vector3"/> to compare.</param>
+		/// <param name="Tolerance">The range that is considered if From has 'reached' To.</param>
+		/// <returns>True if the distance between From and To are &lt;= Tolerance.</returns>
+		public static bool HasReached(Vector3 From, Vector3 To, float Tolerance = .1f)
+		{
+			Vector3 Difference = From - To;
 
-        /// <summary>If the distance between from and to is less than or EqualTo detection.</summary>
-        /// <param name="vFrom">The reference coordinate to compare.</param>
-        /// <param name="vTo">The target coordinate to compare.</param>
-        /// <param name="bEqualTo">If this calculation needs to check if the distance between from and to are equal.</param>
-        /// <param name="fDistance">The range that is considered if from has 'reached' to.</param>
-        /// <returns></returns>
-        public static bool HasReached(Vector3 vFrom, Vector3 vTo, bool bEqualTo = false, float fDistance = .1f) {
-            if (bEqualTo)
-                return Vector3.Distance(vFrom, vTo) < fDistance;
-            return Vector3.Distance(vFrom, vTo) <= fDistance;
-        }
+			return Difference.x <= Tolerance && Difference.y <= Tolerance && Difference.z <= Tolerance;
+		}
 
-        /// <summary>Moves self towards target while moving at velocity with a maximum turn angle of TurnRadius degrees.</summary>
-        /// <param name="RSelf">The Rigidbody to move.</param>
-        /// <param name="ATarget">The Transform destination.</param>
-        /// <param name="fVelocity">The rate at which self moves towards target.</param>
-        /// <param name="fTurnRadius">The maximum degrees self can turn towards target.</param>
-        public static void HomeTowards(Rigidbody RSelf, Transform ATarget, float fVelocity, float fTurnRadius) {
+		/// <summary>Moves Rigidbody towards target while moving at velocity with a maximum turn angle of MaxDegreesDeltaPerFrame.</summary>
+		/// <param name="Rigidbody">The <see cref="Rigidbody"/> to move.</param>
+		/// <param name="Target">The <see cref="Transform"/> destination.</param>
+		/// <param name="Velocity">The rate at which self moves towards target.</param>
+		/// <param name="MaxDegreesDeltaPerFrame">The maximum degrees self can turn towards target per frame.</param>
+		public static void HomeTowards(Rigidbody Rigidbody, Transform Target, float Velocity, float MaxDegreesDeltaPerFrame)
+		{
+			HomeTowards(Rigidbody, Target.position, Velocity, MaxDegreesDeltaPerFrame);
+		}
 
-            Transform _self = RSelf.transform;
-            RSelf.velocity = _self.forward * fVelocity * Time.deltaTime;
-            RSelf.MoveRotation(Quaternion.RotateTowards(_self.rotation, Quaternion.LookRotation(ATarget.position - _self.position), fTurnRadius));
-        }
+		/// <summary>Moves Rigidbody towards target while moving at velocity with a maximum turn angle of MaxDegreesDeltaPerFrame.</summary>
+		/// <param name="Rigidbody">The <see cref="Rigidbody"/> to move.</param>
+		/// <param name="Target">The destination coordinates.</param>
+		/// <param name="Velocity">The rate at which self moves towards target.</param>
+		/// <param name="MaxDegreesDeltaPerFrame">The maximum degrees self can turn towards target per frame.</param>
+		public static void HomeTowards(Rigidbody Rigidbody, Vector3 Target, float Velocity, float MaxDegreesDeltaPerFrame)
+		{
+			Transform _self = Rigidbody.transform;
+			Rigidbody.velocity = _self.forward * Velocity * Time.deltaTime;
+			Rigidbody.MoveRotation(Quaternion.RotateTowards(_self.rotation, Quaternion.LookRotation(Target - _self.position, _self.up), MaxDegreesDeltaPerFrame));
+		}
 
-        /// <summary>Moves self towards target while moving at velocity with a maximum turn angle of TurnRadius degrees.</summary>
-        /// <param name="RSelf">The Rigidbody to move.</param>
-        /// <param name="vTarget">The destination coordinates.</param>
-        /// <param name="fVelocity">The rate at which self moves towards target.</param>
-        /// <param name="fTurnRadius">The maximum degrees self can turn towards target.</param>
-        public static void HomeTowards(Rigidbody RSelf, Vector3 vTarget, float fVelocity, float fTurnRadius) {
+		/// <summary>Moves Rigidbody towards target while moving at velocity with a maximum turn angle of MaxDegreesDeltaPerFrame.</summary>
+		/// <param name="Rigidbody">The <see cref="Rigidbody2D"/> to move.</param>
+		/// <param name="Target">The <see cref="Transform"/> destination.</param>
+		/// <param name="Velocity">The rate at which self moves towards target.</param>
+		/// <param name="MaxDegreesDeltaPerFrame">The maximum degrees self can turn towards target per frame.</param>
+		public static void HomeTowards(Rigidbody2D Rigidbody, Transform Target, float Velocity, float MaxDegreesDeltaPerFrame)
+		{
+			HomeTowards(Rigidbody, Target.position, Velocity, MaxDegreesDeltaPerFrame);
+		}
 
-            Transform _self = RSelf.transform;
-            RSelf.velocity = _self.forward * fVelocity * Time.deltaTime;
-            RSelf.MoveRotation(Quaternion.RotateTowards(_self.rotation, Quaternion.LookRotation(vTarget - _self.position, _self.up), fTurnRadius));
-        }
-    }
+		/// <summary>Moves Rigidbody towards target while moving at velocity with a maximum turn angle of MaxDegreesDeltaPerFrame.</summary>
+		/// <param name="Rigidbody">The <see cref="Rigidbody2D"/> to move.</param>
+		/// <param name="Target">The destination coordinates.</param>
+		/// <param name="Velocity">The rate at which self moves towards target.</param>
+		/// <param name="MaxDegreesDeltaPerFrame">The maximum degrees self can turn towards target per frame.</param>
+		public static void HomeTowards(Rigidbody2D Rigidbody, Vector3 Target, float Velocity, float MaxDegreesDeltaPerFrame)
+		{
+			Transform _Self = Rigidbody.transform;
+			Rigidbody.velocity = _Self.up * Velocity * Time.deltaTime;
+			Rigidbody.MoveRotation(Quaternion.RotateTowards(_Self.rotation, Quaternion.LookRotation(Target - _Self.position, -_Self.forward), MaxDegreesDeltaPerFrame));
+		}
+	}
 }
