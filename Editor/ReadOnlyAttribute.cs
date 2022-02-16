d62028f17a0b1ba83fa70ba8f95cc586eb7cbadf
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-02-16 18:56:30 +1100

    ReadOnly Attribute

---
+++ Editor/ReadOnlyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MW.Editor
+{
+	/// <summary>Sets this property to be visible in the Unity Editor, but is not editable.</summary>
+	public class ReadOnlyAttribute : PropertyAttribute
+	{
+	}
+}
---
+++ Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace MW.Editor
+{
+	[CustomPropertyDrawer(typeof(ReadOnlyAttribute)), SerializeField]
+	public class ReadOnlyDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			bool PreviousGUIState = GUI.enabled;
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label);
+
+			GUI.enabled = PreviousGUIState;
+		}
+	}
+}
