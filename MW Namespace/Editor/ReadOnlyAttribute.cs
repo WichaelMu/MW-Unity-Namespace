6785e84a69c94d670fe35753219a55841b037e40
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-02-26 11:48:39 +1100

    Removed Editor Namespace
    Projects failed to build with MW.Editor.

--- MW Namespace/Editor/ReadOnlyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace MW.Editor
-{
-	/// <summary>Sets this property to be visible in the Unity Editor, but is not editable.</summary>
-	public class ReadOnlyAttribute : PropertyAttribute
-	{
-	}
-}
