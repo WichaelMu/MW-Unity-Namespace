fb352322446dadeac396bde4b56f7ee68506e476
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-02-27 20:19:00 +1100

    MW.Audio Refactor
    MAudio and MSound have been refactored out into their own files.

--- MW Namespace/Audio/MAudio.cs
+++ MW Namespace/Audio/MAudio.cs
@@ -1,50 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using MW.Diagnostics;
 
 namespace MW.Audio
 {
-	/// <summary>The settings for audio.</summary>
-	[Serializable]
-	public class MSound
-	{
-		/// <summary>The source of this sound.</summary>
-		[Tooltip("The source of this sound.")]
-		public AudioClip Source;
-		/// <summary>The name of this sound.</summary>
-		[Tooltip("The name of the sound.")]
-		public string Name;
-
-		/// <summary>Sound settings.</summary>
-		[Range(0f, 1f)]
-		public float Volume = 1, Pitch = 1;
-
-		/// <summary>Should this sound loop?</summary>
-		[Tooltip("Should this audio clip loop?")]
-		public bool bLoop;
-
-		[HideInInspector]
-		public AudioSource AudioSource;
-	}
-
 	/// <summary>The Audio controller for in-game sounds.</summary>
 	public class MAudio : MonoBehaviour
 	{
 		/// <summary>A unique reference to the only Audio object in the scene.</summary>
 		public static MAudio AudioInstance { get => _AudioInstance; set => _AudioInstance = value; }
-		static MAudio _AudioInstance;
+		internal static MAudio _AudioInstance;
 
-		/// <summary>Whether or not to mute every sound by default.</summary>
-		[Tooltip("Whether or not to mute every sound by default.")]
-		public bool bMuteAllByDefault;
 		/// <summary>Every sound that this Audio object will control.</summary>
 		[Tooltip("Every sound that this Audio object will control.")]
 		public MSound[] Sounds;
 
-		const string kErr1 = "Sound of name: ";
-		const string kErr2 = " could not be ";
+		internal Dictionary<string, MSound> Internal_MSound;
+
+		internal const string kErr1 = "Sound of name: ";
+		internal const string kErr2 = " could not be ";
 
 		/// <summary>Populates the Sounds array to match the settings.</summary>
+		/// <remarks>If a duplicate name is detected, a failure will occur. In this case,
+		/// check the names of the MSounds array.
+		/// </remarks>
 		/// <param name="Sounds">The sounds to initialise into the game.</param>
 		public virtual void Initialise(MSound[] Sounds)
 		{
@@ -59,17 +39,53 @@ namespace MW.Audio
 				Destroy(gameObject);
 			}
 
+			if (Internal_MSound == null)
+			{
+				Internal_MSound = new Dictionary<string, MSound>();
+			}
+
 			this.Sounds = Sounds;
 
-			if (!bMuteAllByDefault)
-				foreach (MSound s in Sounds)
+			foreach (MSound s in Sounds)
+			{
+				s.AudioSource = gameObject.AddComponent<AudioSource>();
+				s.AudioSource.clip = s.Source;
+				s.AudioSource.volume = s.Volume;
+				s.AudioSource.pitch = s.Pitch;
+				s.AudioSource.loop = s.bLoop;
+				s.AudioSource.playOnAwake = s.bPlayOnAwake;
+				s.AudioSource.mute = s.bMute;
+
+				try
 				{
-					s.AudioSource = gameObject.AddComponent<AudioSource>();
-					s.AudioSource.clip = s.Source;
-					s.AudioSource.volume = s.Volume;
-					s.AudioSource.pitch = s.Pitch;
-					s.AudioSource.loop = s.bLoop;
+					Internal_MSound.Add(s.Name, s);
 				}
+				catch (ArgumentException)
+				{
+					Log.W("An MSound with Name:", s.Name, "already exists!", "\nChoose another name.");
+				}
+				finally
+				{
+					// When a duplicate Name is detected, add a _FAILURE_ with a count at the
+					// end of the name to show that there was a failure.
+					// Sometimes, there may be multiple duplicates, so keep going until there
+					// is a valid 'spot' for failure.
+
+					uint FailureCount = 1;
+					while (true)
+					{
+						try
+						{
+							Internal_MSound.Add(s.Name + "_FAILURE_" + FailureCount, s);
+							break;
+						}
+						catch (ArgumentException)
+						{
+							FailureCount++;
+						}
+					}
+				}
+			}
 		}
 
 		/// <summary>Plays sound of name n.</summary>
@@ -77,8 +93,6 @@ namespace MW.Audio
 		/// <param name="bOverlapSound"></param>
 		public void Play(string Name, bool bOverlapSound = false)
 		{
-			if (bMuteAllByDefault)
-				return;
 			MSound s = Find(Name);
 			if (s != null && (bOverlapSound || !IsPlaying(s)))
 				s.AudioSource.Play();
@@ -90,8 +104,6 @@ namespace MW.Audio
 		/// <param name="Name">The name of the requested sound to stop playing.</param>
 		public void Stop(string Name)
 		{
-			if (bMuteAllByDefault)
-				return;
 			MSound s = Find(Name);
 			if (s != null && IsPlaying(s))
 				s.AudioSource.Stop();
@@ -102,27 +114,39 @@ namespace MW.Audio
 		/// <summary>Stop every sound in the game.</summary>
 		public void StopAll()
 		{
-			if (bMuteAllByDefault)
-				return;
 			foreach (MSound s in Sounds)
 				s.AudioSource.Stop();
 		}
 
 		/// <summary>Returns a sound in the Sounds array.</summary>
 		/// <param name="Name">The name of the requested sound.</param>
-		/// <returns>The MSound of the requested sound.</returns>
+		/// <returns>The MSound of the requested sound. Null if Name could not be found.</returns>
 
-		public MSound Find(string Name) => Array.Find(Sounds, sound => sound.Name == Name);
+		public MSound Find(string Name)
+		{
+			try
+			{
+				return Internal_MSound[Name];
+			}
+			catch (KeyNotFoundException)
+			{
+				return null;
+			}
+		}
 
 		/// <summary>Whether or not sound of name n is playing.</summary>
 		/// <param name="Name">The name of the sound to query.</param>
 		public bool IsPlaying(string Name)
 		{
-			if (bMuteAllByDefault)
-				return false;
-			return Find(Name).AudioSource.isPlaying;
+			MSound S = Find(Name);
+
+			if (S != null)
+				return S.AudioSource.isPlaying;
+
+			Log.W(kErr1 + Name + kErr2 + "couldn't be found!");
+			return false;
 		}
 
-		static bool IsPlaying(MSound s) => s.AudioSource.isPlaying;
+		internal static bool IsPlaying(MSound s) => s.AudioSource.isPlaying;
 	}
 }
---
+++ MW Namespace/Audio/MSound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace MW.Audio
+{
+	/// <summary>The settings for audio.</summary>
+	[Serializable]
+	public class MSound
+	{
+		/// <summary>The source of this sound.</summary>
+		[Tooltip("The source of this sound.")]
+		public AudioClip Source;
+		/// <summary>The name of this sound.</summary>
+		[Tooltip("The name of the sound.")]
+		public string Name;
+
+		/// <summary>Sound settings.</summary>
+		[Range(0f, 1f)]
+		public float Volume = 1, Pitch = 1;
+
+		/// <summary>Should this sound loop?</summary>
+		[Tooltip("Should this audio clip loop?")]
+		public bool bLoop;
+
+		/// <summary>Should this audio clip automatically play on awake?</summary>
+		[Tooltip("Should this audio clip automatically play on awake?")]
+		public bool bPlayOnAwake;
+
+		/// <summary>Should this audio clip mute?</summary>
+		[Tooltip("Mute the audio source?")]
+		public bool bMute;
+
+		[HideInInspector]
+		public AudioSource AudioSource;
+	}
+}
