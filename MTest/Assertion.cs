ad3154f236afd85405b93778753e9500eaeac7a3
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-06-21 13:36:53 +1000

    MVector Bitshift and Swap Tests / Docs.

---
+++ MTest/Assertion.cs
@@ -0,0 +1,43 @@
+﻿using MTest.Output;
+using MW;
+using UnityEngine;
+
+namespace MTest
+{
+	internal class Assertion
+	{
+		public static bool Assert(int TestNumber, bool bCondition, ref int Passed, string Operation = "")
+		{
+			if (!bCondition)
+			{
+				O.Failed(TestNumber, true.ToString(), false.ToString(), Operation);
+			}
+			else
+			{
+				Passed++;
+			}
+
+			return bCondition;
+		}
+
+		public static void AssertEquals<T>(int TestNumber, T Result, T Expected, ref int Passed, string Operation = "")
+		{
+			if (Assert(TestNumber, Result != null, ref Passed, Operation + " L is null."))
+				return;
+			if (Assert(TestNumber, Expected != null, ref Passed, Operation + " R is null."))
+				return;
+
+			if (Result != null && Expected != null)
+			{
+				if (!Result.Equals(Expected))
+				{
+					O.Failed(TestNumber, Result.ToString() + " == " + Expected.ToString(), false.ToString(), Operation, Result, Expected);
+				}
+				else
+				{
+					Passed++;
+				}
+			}
+		}
+	}
+}
---
+++ MTest/Tolerance.cs
@@ -0,0 +1,35 @@
+﻿using MTest.Output;
+using MW;
+using UnityEngine;
+
+namespace MTest
+{
+	internal static class Tolerance
+	{
+		public static void FloatToleranceCheck(int TestNumber, float L, float R, float T, string Operation, ref int Passed)
+		{
+			if (Mathf.Abs(L - R) > T)
+				O.Failed(TestNumber, L, R, Operation);
+			else
+				Passed++;
+		}
+
+		public static void VectorToleranceCheck(int TestNumber, MVector M, Vector3 U, float T, string Operation, ref int Passed)
+		{
+			float X = M.X - U.x;
+			float Y = M.Y - U.y;
+			float Z = M.Z - U.z;
+
+			X = Mathf.Abs(X);
+			Y = Mathf.Abs(Y);
+			Z = Mathf.Abs(Z);
+
+			bool bFailed = X > T || Y > T || Z > T;
+
+			if (bFailed)
+				O.Failed(TestNumber, U, M, Operation);
+			else
+				Passed++;
+		}
+	}
+}
