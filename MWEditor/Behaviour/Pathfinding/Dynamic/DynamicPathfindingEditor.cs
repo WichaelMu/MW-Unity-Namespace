3116e1efe22cc405c5f6cf86fa3da06b82b12747
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-11-24 13:14:54 +1100

    Custom Editor Module
    MW finally has custom Editor scripts.
    
    For its introduction, MWEditor allows Dynamic Pathfinding the ability to Precompute Nodes in the Unity Editor, out of runtime.

---
+++ MWEditor/Behaviour/Pathfinding/Dynamic/DynamicPathfindingEditor.cs
@@ -0,0 +1,26 @@
+﻿using MW.Pathfinding;
+using UnityEngine;
+using UnityEditor;
+
+namespace MEditor.Pathfinding
+{
+	[CustomEditor(typeof(DynamicPathfinding))]
+	public class DynamicPathfindingEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DynamicPathfinding DP = (DynamicPathfinding)target;
+
+			if (GUILayout.Button("Precompute Nodes"))
+			{
+				DP.PlotPoints();
+				DP.ForceReconnectAll();
+			}
+
+			if (GUILayout.Button("Flush"))
+				DP.Nodes.Flush();
+
+			DrawDefaultInspector();
+		}
+	}
+}
