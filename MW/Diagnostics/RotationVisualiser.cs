2d7a41a8176e6a2c08452b8b8c1b5c62b9bd5553
Michael Wu <69779024+WichaelMu@users.noreply.github.com>
2023-05-31 14:00:18 +1000

    Make Rotation Visualiser

---
+++ MW/Diagnostics/RotationVisualiser.cs
@@ -0,0 +1,21 @@
+﻿using MW.Debugger;
+using UnityEngine;
+
+namespace MW.Diagnostics
+{
+	[ExecuteInEditMode]
+	public class RotationVisualiser : MonoBehaviour
+	{
+		void OnDrawGizmos()
+		{
+			Gizmos.color = Color.red;
+			Arrow.GizmoArrow(transform.position, transform.right, .1f, 45f);
+
+			Gizmos.color = Color.green;
+			Arrow.GizmoArrow(transform.position, transform.up, .1f, 45f);
+
+			Gizmos.color = Color.blue;
+			Arrow.GizmoArrow(transform.position, transform.forward, .1f, 45f);
+		}
+	}
+}
